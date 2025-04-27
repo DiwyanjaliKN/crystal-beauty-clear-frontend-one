b1edca0e030c9bc66a5bb938b024c0e7dfb46b01
KNDiwyanjali <kndiwya@gmail.com>
2025-04-27 16:13:44 +0530

    Day 12✏️

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Routes,Route} from 'react-router-dom';
 import './App.css';
 import AdminPage from './pages/adminPage';
 import LoginPage from './pages/loginPage';
+import Testing from './pages/testing';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <BrowserRouter>
+    <Toaster position= "top-right"/>
       <Routes path="/*">
         <Route path ="/admin/*" element={<AdminPage/>}/>
         <Route path ="/login" element={<LoginPage/>}/>
-        <Route path ="/" element={<hi>Home</hi>}/>
-        <Route path ="/*" element={<hi>404 Not Found ⁉️</hi>}/>
+        <Route path="/testing" element={<Testing />} />
+        <Route path ="/" element={<h1>Home</h1>}/>
+        <Route path ="/*" element={<h1>404 Not Found ⁉️</h1>}/>
       </Routes>
 
     </BrowserRouter>
@@ -18,4 +22,6 @@ function App() {
 }
 
 export default App;
+
+
 
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -19,7 +19,48 @@
     )
 }*/
 
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate=useNavigate()
+
+    function handleLogin() {
+        console.log("Email:", email);
+        console.log("Password:", password);
+
+        axios.post(import.meta.env.VITE_BACKEND_URL+"/api/user/login",{
+            email:email,
+            password:password
+        }).then(
+            (response)=>{
+                console.log("Login successful",response.data);
+                toast.success("Login successful");
+                localStorage.setItem("token",response.data.token);
+                
+                const user = response.data.user;
+
+                if(user.role=="admin"){
+                    navigate("/admin")
+                }else{
+                    navigate("/")
+                }
+                
+        }).catch(
+            (error)=>{
+                console.log("Login Failed",error.response.data);
+                toast.error(error.response.data.message || "Login Failed");
+            }
+        )
+
+
+        console.log("Login button clicked");
+    }
+
     return (
         <div className="w-full h-screen bg-[url('/login.bg.jpg')] bg-cover bg-center flex">
             <div className="w-[50%] h-full">
@@ -29,24 +70,31 @@ export default function LoginPage() {
             <div className="w-[50%] h-full flex justify-center items-center">
                 <div className="w-[450px] h-[650px] backdrop-blur-xl shadow-xl rounded-xl flex flex-col justify-center items-center p-8">
                     <h2 className="text-2xl font-semibold text-white mb-6">Welcome Back</h2>
+
                     <input
-                        className="w-[400px] h-[50px] border border-white[800] rounded-xl text-center text-white  m-[5px] focus:outline-none focus:ring-2 focus:ring-green-400"
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="w-[400px] h-[50px] border border-white rounded-xl text-center text-white m-[5px] focus:outline-none focus:ring-2 focus:ring-green-400"
                         type="email"
                         placeholder="Email"
                         aria-label="Email"
                     />
+
                     <input
+                        onChange={(e) => setPassword(e.target.value)}
                         className="w-[400px] h-[50px] border border-white rounded-xl text-center text-white m-[5px] focus:outline-none focus:ring-2 focus:ring-green-400"
                         type="password"
                         placeholder="Password"
                         aria-label="Password"
                     />
-                    <button
+
+                    <button 
+                        onClick={handleLogin} 
                         className="w-[400px] h-[50px] bg-green-200 text-black rounded-xl cursor-pointer mt-5"
                         aria-label="Login Button"
                     >
                         Login
                     </button>
+
                     <a
                         href="/forgot-password"
                         className="text-green-200 mt-4 text-sm hover:underline"
